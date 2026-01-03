fc149120b4152e555b7f1306f1a76e78ce937c4a
Igor-off <igor.silva.rafael.701@ufrn.edu.br>
2026-01-03 18:26:28 -0300

    Implementação do movimento do personagem no eixo x e mudança no sentido

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,32 +1,44 @@
 extends CharacterBody2D
 
+#=== Referências dos nós
+@onready var animation : AnimatedSprite2D = $Animation
+
 #========== Componente de movimento do Player ==========
 #=== Movimento no eixo x
-@export var max_speed = 100.0
+var direction             = 0
+@export var maximum_speed = 100.0
+@export var acceleration  = 400.0
+@export var deceleration  = 400.0
+
+# Função para virar a animação do Player para esquerda ou para direita
+func update_direction():
+	direction = Input.get_axis("LEFT", "RIGHT")
+	if direction < 0:
+		animation.flip_h = true
+	elif direction > 0:
+		animation.flip_h = false
 
 # Função para atualizar a velocidade no eixo x
-func move_x(_delta):
-	# Verificar se algum botão de movimento foi pressionado
-	var direction := Input.get_axis("LEFT", "RIGHT")
-	if direction:
-		velocity.x = (direction * max_speed)
+func move_x(delta):
+	# Atualizar sentido da direção do movimento do Player no eixo x
+	update_direction()
+	if direction: 
+		# Se sim, acelerar para andar
+		velocity.x = move_toward(velocity.x, (direction * maximum_speed), (acceleration * delta))
 	else:
-		# Parar gradativamente
-		velocity.x = move_toward(velocity.x, 0, max_speed)
+		# Se não, desacelerar e parar
+		velocity.x = move_toward(velocity.x, 0, (deceleration * delta))
 
 #=== Movimento no eixo y
-@export var jump_velocity = -300.0
-
-# Função que aplica velocidade no pulo
-
-func _physics_process(delta: float) -> void:
-	# Aplicar gravidade
+# Função para aplicar a gravidade sobre o Player
+func apply_gravity(delta):
 	if not is_on_floor():
-		velocity += get_gravity() * delta
-	# Mover no eixo x
-	move_x(delta)
-	# Verificar confdição de pulo
-	if Input.is_action_just_pressed("UP") and is_on_floor():
-		velocity.y = jump_velocity
+		velocity += (get_gravity() * delta)
 
+#======================================================
+
+# Processo que aplicara as funções a cada frame
+func _physics_process(delta: float) -> void:
+	apply_gravity(delta)
+	move_x(delta)
 	move_and_slide()
