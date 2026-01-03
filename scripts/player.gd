f55e0dded5bf155dec3a02adef368419803a569f
Igor-off <igor.silva.rafael.701@ufrn.edu.br>
2026-01-03 19:10:34 -0300

    Implementação da maquina de estado

--- scripts/player.gd
+++ scripts/player.gd
@@ -38,16 +38,84 @@ func apply_gravity(delta):
 		velocity += (get_gravity() * delta)
 		
 
-# Função para pular
-func jump(_delta):
+#======================================================
+#========== Máquina de estados do personagem ==========
+#=== Estados do personagem
+enum PlayerState {
+	IDLE = 0, # Parado sem receber comandos de entrada
+	WALKING,  # Movendo-se no eixo X
+	JUMPING,  # Movendo-se no eixo y
+}
+var status: PlayerState # Variável para guardar o estado atual do Player
+#=== Funções de entrada no estado
+# Função para entrar no estado IDLE
+func go_to_idle_state():
+	status = PlayerState.IDLE # Mudar o estao do personagem
+	animation.play("IDLE")    # Mudar a anmação do personagem
+
+# Função para entrar no estado WALKING
+func go_to_walking_state():
+	status = PlayerState.WALKING
+	animation.play("WALKING")
+
+# Função para entrar no estado JUMPING
+func go_to_jumping_state():
+	status = PlayerState.JUMPING
+	animation.play("JUMPING")
+	velocity.y = jump_speed
+
+#=== Funções de processamento do estado atual
+# Função para processar o estado IDLE
+func idle_state(delta):
+	# Enquanto estiver no estado:
+	apply_gravity(delta) # Aplicar a gravidade
+	move_x(delta)        # Manter o personagem parado
+	# Mudar estado
 	if Input.is_action_just_pressed("UP"):
-		velocity.y = jump_speed
+		go_to_jumping_state()
+		return
+	if (velocity.x != 0):
+		go_to_walking_state()
+		return
+
+func walking_state(delta):
+	apply_gravity(delta)
+	move_x(delta)
+	# Mudar estado
+	if velocity.x == 0:
+		go_to_idle_state()
+		return
+	if Input.is_action_just_pressed("UP"):
+		go_to_jumping_state()
+		return
+
+func jumping_state(delta):
+	apply_gravity(delta)
+	move_x(delta)
+	# Mudar estado
+	if is_on_floor():
+		# Cair parado
+		if velocity.x == 0:
+			go_to_idle_state()
+		# Cair correndo
+		else:
+			go_to_walking_state()
+		return
 
 #======================================================
 
-# Processo que aplicara as funções a cada frame
+# Método de inicialização do estado
+func _ready() -> void:
+	go_to_idle_state() # Inicializar no estado IDLE
+
+# Processo para processar o estado a cada frame
 func _physics_process(delta: float) -> void:
-	apply_gravity(delta)
-	move_x(delta)
-	jump(delta)
+	# Capturar os estados a cada frame
+	match status:
+		PlayerState.IDLE:
+			idle_state(delta)
+		PlayerState.WALKING:
+			walking_state(delta)
+		PlayerState.JUMPING:
+			jumping_state(delta)
 	move_and_slide()
